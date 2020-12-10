a249666556024100c5ffb63f0beabefde6bc47c9
Gon Gis <g0ng0n.dev@gmail.com>
2020-12-10 15:17:56 +0100

    moving from class component to function component in ManageCoursePage

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from 'prop-types'
+
+function ManageCoursePage ({ courses, authors, loadAuthors, loadCourses }) {
+
+    useEffect( () => {
+        if (courses.length === 0){
+            loadCourses().catch(error => {
+                alert("Loading courses failed " + error);
+            })
+        }
+        if (authors.length === 0) {
+            loadAuthors().catch(error => {
+                alert("Loading Authors failed " + error);
+            })
+        }
+    },[]);
+
+    return (
+            <>
+                <h2>Manage Course</h2>
+            </>
+        )
+
+}
+
+ManageCoursePage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    authors: PropTypes.array.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state){
+    return {
+        courses: state.courses,
+        authors: state.authors,
+    };
+}
+
+const mapDispatchToProps = {
+    loadCourses: courseActions.loadCourses,
+    loadAuthors: authorActions.loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
