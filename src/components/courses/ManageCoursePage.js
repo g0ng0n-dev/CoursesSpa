70110ef42a1569b3dbb9a1cb40b4a4cd09b01823
Gon Gis <g0ng0n.dev@gmail.com>
2020-12-10 16:18:11 +0100

    adding code implementation for redux flow for saving course

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -8,8 +8,7 @@ import { newCourse } from '../../../tools/mockData';
 
 function ManageCoursePage ({
                                courses,
-                               authors, loadAuthors,
-                               loadCourses,
+                               authors,
                                ...props
 }) {
     const [ course, setCourse ] = useState({...props.course});
@@ -17,21 +16,38 @@ function ManageCoursePage ({
 
     useEffect( () => {
         if (courses.length === 0){
-            loadCourses().catch(error => {
+            courseActions.loadCourses().catch(error => {
                 alert("Loading courses failed " + error);
             })
         }
         if (authors.length === 0) {
-            loadAuthors().catch(error => {
+            authorActions.loadAuthors().catch(error => {
                 alert("Loading Authors failed " + error);
             })
         }
     },[]);
 
-    return (
-            <CourseForm course={course} errors={errors} authors={authors} />
-        )
+   function handleChange(event){
+        const { name, value } = event.target;
+        setCourse(prevCourse => ({
+            ...prevCourse,
+            [name]: name === 'authorId' ? parseInt(value, 10): value
+        }))
+    }
 
+    function handleSave(event){
+        event.preventDefault();
+        courseActions.saveCourse(course);
+    }
+
+    return (
+            <CourseForm
+                course={course}
+                onChange={handleChange}
+                onSave={handleSave}
+                errors={errors}
+                authors={authors} />
+        );
 }
 
 ManageCoursePage.propTypes = {
@@ -39,7 +55,8 @@ ManageCoursePage.propTypes = {
     courses: PropTypes.array.isRequired,
     authors: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
-    loadAuthors: PropTypes.func.isRequired,
+    saveCourse: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state){
@@ -53,6 +70,7 @@ function mapStateToProps(state){
 const mapDispatchToProps = {
     loadCourses: courseActions.loadCourses,
     loadAuthors: authorActions.loadAuthors,
+    saveCourse: courseActions.saveCourse,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
